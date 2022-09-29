f3c5d0119df3377e7cd17f5fceddb42ee60233e4
Antro Safin <85235476+AntroSafin@users.noreply.github.com>
2022-09-29 19:08:37 +0530

    Update 724. Find Pivot Index.py

--- Python/724. Find Pivot Index.py
+++ Python/724. Find Pivot Index.py
@@ -1,59 +1,12 @@
-"""
-Given an array of integers nums, calculate the pivot index of this array.
-
-The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
-
-If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
-
-Return the leftmost pivot index. If no such index exists, return -1.
-
- 
-
-Example 1:
-
-Input: nums = [1,7,3,6,5,6]
-Output: 3
-Explanation:
-The pivot index is 3.
-Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
-Right sum = nums[4] + nums[5] = 5 + 6 = 11
-
-Example 2:
-
-Input: nums = [1,2,3]
-Output: -1
-Explanation:
-There is no index that satisfies the conditions in the problem statement.
-
-Example 3:
-
-Input: nums = [2,1,-1]
-Output: 0
-Explanation:
-The pivot index is 0.
-Left sum = 0 (no elements to the left of index 0)
-Right sum = nums[1] + nums[2] = 1 + -1 = 0
-"""
-
-#SOLUTION:
-def pivotIndex(self, nums: List[int]) -> int:
-    l = [0]*len(nums)
-    r = [0]*len(nums)
-    for i in range(1,len(nums)):
-        l[i] += l[i-1] + nums[i-1]
-    for i in range(len(nums)-2,-1,-1):
-        r[i] += r[i+1] + nums[i+1]
-    for i in range(len(nums)):
-        if(l[i] == r[i]):
-            return i
-    return -1
-
-
- def pivotIndex(self, nums):
-    S = sum(nums)
-    leftsum = 0
-    for i, x in enumerate(nums):
-        if leftsum == (S - leftsum - x):
-            return i
-        leftsum += x
-    return -1
+class Solution:
+ def pivotIndex(self, nums: List[int]) -> int:
+     l = [0]*len(nums)
+     r = [0]*len(nums)
+     for i in range(1,len(nums)):
+         l[i] += l[i-1] + nums[i-1]
+     for i in range(len(nums)-2,-1,-1):
+         r[i] += r[i+1] + nums[i+1]
+     for i in range(len(nums)):
+         if(l[i] == r[i]):
+             return i
+     return -1

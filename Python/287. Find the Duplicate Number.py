a80fc56ab0912d5764b10a820abb5fd699bf73a7
Antro Safin <85235476+AntroSafin@users.noreply.github.com>
2022-09-29 19:04:50 +0530

    Update 287. Find the Duplicate Number.py

--- Python/287. Find the Duplicate Number.py
+++ Python/287. Find the Duplicate Number.py
@@ -1,47 +1,7 @@
-"""
-Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
-
-There is only one repeated number in nums, return this repeated number.
-
-You must solve the problem without modifying the array nums and uses only constant extra space.
-
- 
-
-Example 1:
-
-Input: nums = [1,3,4,2,2]
-Output: 2
-
-Example 2:
-
-Input: nums = [3,1,3,4,2]
-Output: 3
-"""
-
-#MY SOLUTION:
-def findDuplicate(self, nums: List[int]) -> int:
-    container = set()
-    for i in nums:
-        if(i in container):
-            return i
-        container.add(i)
-
-
-#OPTIMAL SOLUTION:
-
-def findDuplicate(self, nums: List[int]) -> int:
-    # Find the intersection point of the two runners.
-    tortoise = hare = nums[0]
-    while True:
-        tortoise = nums[tortoise]
-        hare = nums[nums[hare]]
-        if tortoise == hare:
-            break
-    
-    # Find the "entrance" to the cycle.
-    tortoise = nums[0]
-    while tortoise != hare:
-        tortoise = nums[tortoise]
-        hare = nums[hare]
-    
-    return hare
+class Solution:
+ def findDuplicate(self, nums: List[int]) -> int:
+     container = set()
+     for i in nums:
+         if(i in container):
+             return i
+         container.add(i)

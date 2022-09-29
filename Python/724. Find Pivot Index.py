bcf3be80da55ea1ccc1bc4bf670d36df5c55324a
Antro Safin <85235476+AntroSafin@users.noreply.github.com>
2022-09-29 19:15:48 +0530

    Update 724. Find Pivot Index.py

--- Python/724. Find Pivot Index.py
+++ Python/724. Find Pivot Index.py
@@ -1,12 +1,12 @@
 class Solution:
- def pivotIndex(self, nums: List[int]) -> int:
-     l = [0]*len(nums)
-     r = [0]*len(nums)
-     for i in range(1,len(nums)):
-         l[i] += l[i-1] + nums[i-1]
-     for i in range(len(nums)-2,-1,-1):
-         r[i] += r[i+1] + nums[i+1]
-     for i in range(len(nums)):
-         if(l[i] == r[i]):
-             return i
-     return -1
+    def pivotIndex(self, nums: List[int]) -> int:
+        l = [0]*len(nums)
+        r = [0]*len(nums)
+        for i in range(1,len(nums)):
+            l[i] += l[i-1] + nums[i-1]
+        for i in range(len(nums)-2,-1,-1):
+            r[i] += r[i+1] + nums[i+1]
+        for i in range(len(nums)):
+            if(l[i] == r[i]):
+                return i
+        return -1

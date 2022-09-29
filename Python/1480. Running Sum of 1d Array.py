339cf9ea48ddd3d0ff53ff1ac6e9449c4c72e77e
Antro Safin <85235476+AntroSafin@users.noreply.github.com>
2022-09-29 19:25:46 +0530

    Update 1480. Running Sum of 1d Array.py

--- Python/1480. Running Sum of 1d Array.py
+++ Python/1480. Running Sum of 1d Array.py
@@ -1,8 +1,8 @@
 class Solution:
- def runningSum(self, nums: List[int]) -> List[int]:
-     val = 0
-     ans = []
-     for i in nums:
-         val += i
-         ans.append(val)
-     return ans
+    def runningSum(self, nums: List[int]) -> List[int]:
+        val = 0
+        ans = []
+        for i in nums:
+            val += i
+            ans.append(val)
+        return ans

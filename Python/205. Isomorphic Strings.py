54a1f90fce7025671b78578ff5d9c0ff89c0518f
Antro Safin <85235476+AntroSafin@users.noreply.github.com>
2022-09-29 19:24:40 +0530

    Update 205. Isomorphic Strings.py

--- Python/205. Isomorphic Strings.py
+++ Python/205. Isomorphic Strings.py
@@ -1,10 +1,10 @@
 class Solution:
- def isIsomorphic(self, s: str, t: str) -> bool:
-     hp = {}
-     hp1 = {}
-     for i in range(len(t)):
-         if((s[i] in hp and hp[s[i]] != t[i]) or (t[i] in hp1 and hp1[t[i]] != s[i])):
-             return False
-         hp[s[i]] = t[i]
-         hp1[t[i]] = s[i]
-     return True
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        hp = {}
+        hp1 = {}
+        for i in range(len(t)):
+            if((s[i] in hp and hp[s[i]] != t[i]) or (t[i] in hp1 and hp1[t[i]] != s[i])):
+                return False
+            hp[s[i]] = t[i]
+            hp1[t[i]] = s[i]
+        return True
